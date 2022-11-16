7ce87dcabdd70a32ca194b2d79d55f070c994b94
Pattana Sapphanachai <60337808+6210612831@users.noreply.github.com>
2022-11-16 19:03:06 +0700

    STUDENTAPIVIEW

--- apis/urls.py
+++ apis/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from .views import AuthenticationApiView
-
+from .views import StudentDataApiView
 
 app_name = 'apis'
 v1_urls = (
     [
         path('authentication/', AuthenticationApiView.as_view()),
+        path('test', StudentDataApiView.as_view()),
     ],
     'v1')
 
--- apis/views.py
+++ apis/views.py
@@ -8,6 +8,10 @@ from .authentication import ldap3_authen
 # from .models import Task
 # from django.contrib.auth.models import User
 
+# MariadDB connect
+import mariadb
+import sys
+
 
 
 class AuthenticationApiView(APIView):
@@ -20,3 +24,37 @@ class AuthenticationApiView(APIView):
         else:
             return Response('Unauthorized', status=status.HTTP_401_UNAUTHORIZED)
         
+
+class StudentDataApiView(APIView):
+    permission_classes = []
+
+    def get(self, request, *args, **kwargs):
+        # Connect to MariaDB Platform
+        std_code = request.query_params['std_code']
+        txt = ""
+        try:
+            conn = mariadb.connect(
+                user="root",
+                password="toor",
+                host="127.0.0.1",
+                port=3307,
+                database="maraidb_project"
+            )
+            txt = "Connect Complete"
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB Platform: {e}")
+            txt = f"Error connecting to MariaDB Platform: {e}"
+            sys.exit(1)
+
+        # Get Cursor
+        cur = conn.cursor()
+        cur.execute(f"SELECT * FROM student_showdetail3 WHERE std_code={std_code}")
+        fetch_data = cur.fetchone()
+        num_fields = len(fetch_data)
+        output = []
+        for index in range (0,num_fields):
+            output.append({cur.description[index][0]:fetch_data[index]})
+        return Response({'output':output}, status=status.HTTP_200_OK)
+
+
+
