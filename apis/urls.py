98702598854d7301bc6bfd5975edfa23cf7396ce
6210612831 <6210612831@student.tu.ac.th>
2023-01-24 13:22:50 +0700

    api std_gender, api std_adyear

--- apis/urls.py
+++ apis/urls.py
@@ -4,9 +4,22 @@ from .views import StudentDataApiView
 from .views import MajorApiView
 from .views import DepartmentApiView
 from .views import InstructorApiView
+from .views import ReportStdGenderApiView
+from .views import ReportStdAdyearApiView
 # from .views import TestApiView
 
 app_name = 'apis'
+
+report_std_urls = (
+    [
+        path('gender/', ReportStdGenderApiView.as_view()),
+        path('adyear/', ReportStdAdyearApiView.as_view()),
+        
+    ],
+    'report_std')
+
+
+
 v1_urls = (
     [
         path('authentication/', AuthenticationApiView.as_view()),
@@ -15,8 +28,11 @@ v1_urls = (
         path('department', DepartmentApiView.as_view()),
         path('instructor', InstructorApiView.as_view()),
         # path('test', TestApiView.as_view()),
+        path('report/std/', include(report_std_urls)),
     ],
     'v1')
+
+
 
 
 
